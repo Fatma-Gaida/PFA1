7353d59f501e48202461cd987edbe4c4d27dc0e8
Fatma Zahra Gaida <fatmagaida52@gmail.com>
2024-04-07 00:43:22 +0100

    connection with DB done

--- envoi.php
+++ envoi.php
@@ -2,36 +2,161 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-$page =  1 ;
-if( isset($_POST['page1'])){
-  // traitmenet de verification du contenu de la page 
-  // si c'est vrai alors incrémenter variable page et refresh
-  // si c'est faux rester dans la méme page et afficher les erreurs
-  
-  $page = 2 ;
-}
-if (isset($_POST['page2']) && $_POST['page2'] == "Continuer") {
-  // Traitement de vérification du contenu de la page
+$page =  1 ; 
+session_start();
+
+$id_colis = $_SESSION['id_colis'] ?? null;
+$id_client_expediteur = $_SESSION['id_client_expediteur'] ?? null;
+$id_client_destinataire = $_SESSION['id_client_destinataire'] ?? null;
+
+if( $_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['page1'])){
+  // Traitement de vérification du contenu de la page 
   // Si c'est vrai alors incrémenter la variable page et refresh
   // Si c'est faux rester dans la même page et afficher les erreurs
-  $page = 3; // Mettre à jour pour passer à la page suivante
+  $nom_cli = $_POST['nom_ex'];
+  $prenom_cli = $_POST['prenom_ex'];
+  $adresse_cli = $_POST['adresse_ex'];
+  $email_cli = $_POST['email_ex'];
+  $num_tel_cli = $_POST['numtel_ex'];
+  $ville_cli = $_POST['ville_ex'];
+  $code_postal_cli_ex = $_POST['code_postal_cli_ex'];
+  $type_cli = 'expediteur';
+  $_SESSION['pr_ex'] = $_POST['pr_ex'];
+
+  echo "Récupération terminée  ";
+
+  // Informations de connexion à la base de données
+  $host = 'localhost';
+  $dbname = 'mysql';
+  $username = 'root';
+  $password = '';
+
+  try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "ouverture de la base de données  ";
+    $sql = "INSERT INTO client (nom_cli,prenom_cli,type_cli,adresse_cli,email_cli,num_tel_cli,ville_cli,code_postal_cli) VALUES ('$nom_cli', '$prenom_cli', '$type_cli' ,'$adresse_cli', '$email_cli', '$num_tel_cli', '$ville_cli', '$code_postal_cli_ex')";
+
+    $pdo->query($sql);
+
+    $id_client_expediteur = $pdo->lastInsertId();
+    $_SESSION['id_client_expediteur'] = $id_client_expediteur;
+    $page = 2 ;
+  } catch (PDOException $e) {
+      echo "Erreur lors de l'insertion des données : " . $e->getMessage();
+  }
+
 }
 
+echo "$id_client_expediteur ";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['page2']) && $_POST['page2'] == "Continuer") {
+  $nom_cli = $_POST['nom_d'];
+  $prenom_cli = $_POST['prenom_d'];
+  $adresse_cli = $_POST['adresse_d'];
+  $email_cli = $_POST['email_d'];
+  $num_tel_cli = $_POST['numtel_d'];
+  $ville_cli = $_POST['ville_d'];
+  $code_postal_cli_d = $_POST['code_postal_cli_d'];
+  $type_cli = 'destinataire';
+  $_SESSION['pr_d'] = $_POST['pr_d'];
+  echo "Récupération terminée  ";
+
+  // Informations de connexion à la base de données
+  $host = 'localhost';
+  $dbname = 'mysql';
+  $username = 'root';
+  $password = '';
+
+  try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "ouverture de la base de données  ";
+    $sql = "INSERT INTO client (nom_cli,prenom_cli,type_cli,adresse_cli,email_cli,num_tel_cli,ville_cli,code_postal_cli) VALUES ('$nom_cli', '$prenom_cli', '$type_cli' ,'$adresse_cli', '$email_cli', '$num_tel_cli', '$ville_cli', '$code_postal_cli_d')";
+
+    $pdo->query($sql);
+
+    $id_client_destinataire = $pdo->lastInsertId();
+    $_SESSION['id_client_destinataire'] = $id_client_destinataire;
+    $page = 3 ; // Mettre à jour pour passer à la page suivante
+  } catch (PDOException $e) {
+      echo "Erreur lors de l'insertion des données : " . $e->getMessage();
+  }
+
+}
+
+echo "$id_client_destinataire ";
+
 if( isset($_POST['page2']) && $_POST['page2'] == "Retourner"){
-  // traitmenet de verification du contenu de la page 
-  // si c'est vrai alors incrémenter variable page et refresh
-  // si c'est faux rester dans la méme page et afficher les erreurs
+  
   $page = 1 ;
 }
 if( isset($_POST['page3']) && $_POST['page3'] == "Retourner"){
-  // traitmenet de verification du contenu de la page 
-  // si c'est vrai alors incrémenter variable page et refresh
-  // si c'est faux rester dans la méme page et afficher les erreurs
+ 
   $page = 2 ;
 }
-if( isset($_POST['page3']) && $_POST['page3'] == "Continuer"){
-  $page = 4 ;
+if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['page3']) && $_POST['page3'] == "Continuer"){
+  $contenance = $_POST['contenance'];
+  $poids = $_POST['poids'];
+  $largeur = $_POST['largeur'];
+  $longueur = $_POST['longueur'];
+  $description = $_POST['description'];
+  $date_depot = $_POST['date_depot'];
+  $pr_d= $_SESSION['pr_d'];
+  $pr_ex= $_SESSION['pr_ex'];
+
+  echo "Récupération terminée  ";
+
+  // Informations de connexion à la base de données
+  $host = 'localhost';
+  $dbname = 'mysql';
+  $username = 'root';
+  $password = '';
+  echo "$contenance ; $poids ; $largeur ; $longueur ; $description ; $date_depot ; $pr_ex ; $pr_d ";
+ 
+  try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "ouverture de la base de données  ";
+    $sql = "INSERT INTO colis (poids_colis,type_colis,date_depot_colis,position_actuelle,point_relais_initiale,point_relais_finale,longeur_colis,largeur_colis,description) VALUES ('$poids', '$contenance', '$date_depot' ,'$pr_ex','$pr_ex','$pr_d', '$longueur', '$largeur', '$description')";
+    $pdo->query($sql);
+
+    $id_colis = $pdo->lastInsertId();
+    $_SESSION['id_colis'] = $id_colis;
+    $page = 4 ; // Mettre à jour pour passer à la page suivante
+  } catch (PDOException $e) {
+      echo "Erreur lors de l'insertion des données : " . $e->getMessage();
+  }
+
 }
+
+echo "$id_colis";
+
+if( isset($_POST['page4'])){
+  $host = 'localhost';
+  $dbname = 'mysql';
+  $username = 'root';
+  $password = '';
+
+  echo "$id_colis, $id_client_expediteur";
+
+  try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "ouverture de la base de données  ";
+
+    $sql_insert_deposer_recuperer = "INSERT INTO deposer_recuperer (id_colis, id_client) VALUES ('$id_colis','$id_client_expediteur')";
+    $pdo->query($sql_insert_deposer_recuperer);
+
+    $sql = "INSERT INTO deposer_recuperer (id_colis, id_client) VALUES ('$id_colis','$id_client_destinataire')";
+    $pdo->query($sql);
+
+  } catch (PDOException $e) {
+      echo "Erreur lors de l'insertion des données dans la table deposer_recuperer : " . $e->getMessage();
+  }
+}
+
+
 
 if( isset($_POST['page4']) && $_POST['page4'] == "Retourner"){
   $page =  3;
@@ -54,51 +179,6 @@ if( isset($_POST['page5']) && $_POST['page5'] == "Terminer"){
   $page = 6 ;
 }
 
-$nom_cli = $_POST['nom_ex'] ?? '';
-$prenom_cli = $_POST['prenom_ex'] ?? '';
-$adresse_cli = $_POST['adresse_ex'] ?? '';
-$email_cli = $_POST['email_ex'] ?? '';
-$num_tel_cli = $_POST['numtel_ex'] ?? '';
-$ville_cli = $_POST['ville_ex'] ?? '';
-$code_postal = $_POST['code_postal'] ?? '';
-$type_cli = 'expediteur';
-
-// Informations de connexion à la base de données
-$host = 'localhost';
-$dbname = 'mysql';
-$username = 'root';
-$password = '';
-
-try {
-    // Connexion à la base de données MySQL
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // Préparation de la requête d'insertion
-    $sql = "INSERT INTO client (nom_cli, prenom_cli, adresse_cli, email_cli, num_tel_cli, ville_cli, code_postal_cli, type_cli) 
-            VALUES (:nom_cli, :prenom_cli, :adresse_cli, :email_cli, :num_tel_cli, :ville_cli, :code_postal, :type_cli)";
-    $stmt = $pdo->prepare($sql);
-
-    // Liaison des valeurs aux paramètres de la requête
-    $stmt->bindParam(':nom_cli', $nom_cli);
-    $stmt->bindParam(':prenom_cli', $prenom_cli);
-    $stmt->bindParam(':adresse_cli', $adresse_cli);
-    $stmt->bindParam(':email_cli', $email_cli);
-    $stmt->bindParam(':num_tel_cli', $num_tel_cli);
-    $stmt->bindParam(':ville_cli', $ville_cli);
-    $stmt->bindParam(':code_postal', $code_postal);
-    $stmt->bindParam(':type_cli', $type_cli);
-
-    // Exécution de la requête d'insertion
-    $stmt->execute();
-
-    // Redirection vers une page de confirmation
-    header("Location: envoi.php?message=Votre compte a été créé avec succès");
-    exit();
-} catch (PDOException $e) {
-    // En cas d'erreur, affichage du message d'erreur
-    echo "Erreur lors de l'insertion des données : " . $e->getMessage();
-}
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -118,7 +198,7 @@ try {
       <ul>
         <li><a href="acceuil.php">acceuil</a></li>
         <li><a href="#">suivi de colis </a></li>
-        <li><a href="index.php">envoi de colis</a></li>
+        <li><a href="envoi.php">envoi de colis</a></li>
         <li><a href="#">Nos points de relais </a></li>
         <li>
         <div id="loginContainer" style="padding-top: 2.5px;">
@@ -200,18 +280,18 @@ try {
             </div>
             
             <div class="user-box">
-            <input type="text" name="code_postal_cli" required="">
+            <input type="text" name="code_postal_cli_ex" required="">
             <label>Code postal</label>
           </div>
             <label>Point de Relais</label><br>
             <div class="select">
-            <select>
-              <option>First select</option>
-              <option>Option</option>
-              <option>Option</option>
-              <option>Option</option>
-              <option>Option</option>
-              <option>Option</option>
+            <select name="pr_ex">
+              <option value="option1">First select</option>
+              <option value="option2">Option</option>
+              <option value="option3">Option</option>
+              <option value="option4">Option</option>
+              <option value="option5">Option</option>
+              <option value="option6">Option</option>
             </select>
             </div>
             </div></div>
@@ -259,7 +339,7 @@ try {
           </div>
           <div class="part2">
             <div class="user-box">
-              <input type="text" name="prenom-d" required="">
+              <input type="text" name="prenom_d" required="">
               <label>Prénom</label>
             </div>
             <div class="user-box">
@@ -269,18 +349,18 @@ try {
             </div>
         
             <div class="user-box">
-            <input type="text" name="code_postal_cli" required="">
+            <input type="text" name="code_postal_cli_d" required="">
             <label>Code postal</label>
           </div>
             <label>Point de Relais</label><br>
             <div class="select">
-            <select>
-              <option>First select</option>
-              <option>Option</option>
-              <option>Option</option>
-              <option>Option</option>
-              <option>Option</option>
-              <option>Option</option>
+            <select name="pr_d">
+              <option value="option1">First select</option>
+              <option value="option2">Option</option>
+              <option value="option3">Option</option>
+              <option value="option4">Option</option>
+              <option value="option5">Option</option>
+              <option value="option6">Option</option>
             </select>
             </div>
           </div>
